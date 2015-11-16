31f994b1af5241a5ad2eb45524e936ed6c1b1352
Joe Ainsworth <joe.ainsworth@me.com>
2015-11-16 23:25:13 +0000

    Some refactoring and basic authentication.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "joe", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
